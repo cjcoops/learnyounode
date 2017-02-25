ce17e93bd51c17b73dce1670ba3012e648590e2f
Chris Cooper <cjcooper252@gmail.com>
2017-02-25 14:09:17 +0000

    completes ex8

--- program.js
+++ program.js
@@ -4,10 +4,19 @@ let bl = require('bl')
 let http = require('http')
 
 http.get(process.argv[2], (response) => {
-  response.setEncoding('utf8')
-  response.on('data', console.log)
-  response.on('error', console.error)
-}).on('error', console.error)
+  response.pipe(bl((err, data) => {
+    if (err) return console.error(err);
+    let stringData = data.toString()
+    console.log(stringData.length)
+    console.log(stringData)
+  }))
+})
+
+// http.get(process.argv[2], (response) => {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// }).on('error', console.error)
 
 // fs.readdir(process.argv[2], (err, files) => {
 //   if (err) throw err;
